4e8051a7e6b318c71db9157e0e30dfa862dac732
skunkix <pigrsi.boat@gmail.com>
2021-05-22 22:37:53 +0100

    Enable cors on everything for now

--- main.js
+++ main.js
@@ -6,14 +6,16 @@ const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 var whitelist = ['http://localhost:8080'];
-
 const corsOptions = {
-  origin: (origin, callback) => whitelist.indexOf(origin) !== -1 ? 
-    callback(null, true) : 
-    callback(null, false)
+  // origin: (origin, callback) => whitelist.indexOf(origin) !== -1 ? 
+  //   callback(null, true) : 
+  //   callback(null, false)
+    origin: '*'
 };
 
-app.post('/getImages', cors(corsOptions), (req, res) => {
+app.use(cors(corsOptions));
+
+app.post('/getImages', (req, res) => {
   res.send({
     heresSome: "jsonForYa"
   });
