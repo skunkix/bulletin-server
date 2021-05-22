154418ef672944393ce88ba172b5c884bff8683e
skunkix <pigrsi.boat@gmail.com>
2021-05-22 22:16:12 +0100

    Added CORS exception for localhost

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const http = require('http');
 const app = require('express')();
+const cors = require("cors");
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
+
+var whitelist = ['http://localhost:3000'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}));
 
 app.post('/getImages', (req, res) => {
   res.send({
